1988731da0af25febe2b4a1013dca6734ad59411
Mariano <marianolop22@yahoo.com.ar>
2019-10-05 15:38:50 -0300

    environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
 export const environment = {
-  production: true
+  production: true,
+  urlApi: 'http://api.micolegio.com.ar:8080/api/v1',
+  firebaseConfig: {
+
+    apiKey: 'AIzaSyBXRCGCHOakcd6KXx99FswMGw0g9yH4BQs',
+    authDomain: 'school-3e51f.firebaseapp.com',
+    databaseURL: 'https://school-3e51f.firebaseio.com',
+    projectId: 'school-3e51f',
+    storageBucket: 'school-3e51f.appspot.com',
+    messagingSenderId: '439108460621',
+    appId: '1:439108460621:web:ef6f4a53e9a04368',
+  },
+
+  //urls de servicios
+  urlFirebaseLogin: 'https://identitytoolkit.googleapis.com/v1',
 };
